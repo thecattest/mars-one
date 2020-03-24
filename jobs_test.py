570b3cc6f46e0cc6b6bbb31d43d7ae1948c595e7
vodopyanov ilya <vodopyanov999@gmail.com>
2020-03-24 20:58:04 +0300

    edit job api

--- jobs_test.py
+++ jobs_test.py
@@ -37,14 +37,35 @@ def test_link(expected, func="jobs", method="GET", id="0", json={}, print_res=Fa
 
 # test_link(400, method="POST")
 # test_link(400, method="POST", json={"job": "test_job"})
-# test_link(200, method="POST",
-  #        json={
-   #           "job": "test_job",
-    #          "team_leader": 1,
-     #         "work_size": 15,
-      #        "collaborators": "1, 2, 3",
-       #       "kind": "1",
-        #      "is_finished": False})
+test_link(200, method="POST",
+          json={
+              "job": "test_job",
+              "team_leader": 1,
+              "work_size": 15,
+              "collaborators": "1, 2, 3",
+              "kind": "1",
+              "is_finished": False})
+# не указан id
+test_link(400, method="PUT", json={"job": "test"})
+# указан неправильный id
+test_link(404, method="PUT", json={"id": "999", "job": "test"})
 
+# правильный id
+test_link(200, method="PUT", json={"id": "5", "job": "updated test"})
+# проверить, что изменилась
+test_link(200, print_res=True)
+
+# правильный id
+test_link(200, method="PUT", json={"id": "5",
+                                   "job": "updated test again",
+                                   "work_size": "50",
+                                   "collaborators": "4",
+                                   "kind": "2",
+                                   "team_leader": "2",
+                                   "is_finished": True})
+# проверить, что изменилась
+test_link(200, print_res=True)
+
+# удалим эту работу
 # test_link(404, method="DELETE", id="999")
-# test_link(200, method="DELETE", id="5")
+test_link(200, method="DELETE", id="5")
